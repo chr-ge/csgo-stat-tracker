0a77bf687ec594b0218af717f45e3e30f98c0a27
chr-ge <ace26@protonmail.com>
2020-05-24 19:52:12 -0400

    Map style changes

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -13,7 +13,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableRow from '@material-ui/core/TableRow';
 import Avatar from '@material-ui/core/Avatar';
-import ExploreIcon from '@material-ui/icons/Explore';
 
 const Maps = () => {
     const { player, playerMaps, getMaps, error } = useContext(PlayerContext);
@@ -40,16 +39,16 @@ const Maps = () => {
                     <Table aria-label="maps table">
                         <TableHead>
                             <TableRow>
-                                <TableCell align="center" padding="checkbox"><ExploreIcon style={{ verticalAlign: 'middle'}}/></TableCell>
-                                <TableCell align="left" ><Typography variant="h6" color="secondary">Map</Typography></TableCell>
-                                <TableCell align="right"><Typography variant="h6" color="secondary">Rounds</Typography></TableCell>
-                                <TableCell align="right"><Typography variant="h6" color="secondary">Wins</Typography></TableCell>
+                                <TableCell padding="checkbox" align="center" ><Typography variant="subtitle1" color="secondary">Map</Typography></TableCell>
+                                <TableCell></TableCell>
+                                <TableCell align="right"><Typography variant="subtitle1" color="secondary">Rounds</Typography></TableCell>
+                                <TableCell align="right"><Typography variant="subtitle1" color="secondary">Wins</Typography></TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
                         {sortedPlayerMaps.map((map, i) => (
                             <TableRow key={i}>
-                                <TableCell><Avatar src={map.metadata.imageUrl} /></TableCell>
+                                <TableCell align="center"><Avatar src={map.metadata.imageUrl} /></TableCell>
                                 <TableCell component="th" scope="row">{map.metadata.name}</TableCell>
                                 <TableCell align="right">{map.stats.rounds.displayValue}</TableCell>
                                 <TableCell align="right">{map.stats.wins.displayValue}</TableCell>
