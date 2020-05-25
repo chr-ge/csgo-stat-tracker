9b956f71ed5fb89d4072014d2777901293a3d829
chr-ge <ace26@protonmail.com>
2020-05-25 01:00:14 -0400

    changed header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import LogoImage from '../../images/logo.png';
 
+//material-ui
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 
 const Header = () => {
     return (
         <AppBar>
-            <Toolbar>
-                <img src={LogoImage} alt="Logo" width="50" height="50"/>
-                <Typography variant="h4" style={{ marginLeft: 20}}>CS:GO Tracker</Typography>
-            </Toolbar>
+            <Container>
+                <Toolbar>
+                    <img src={LogoImage} alt="Logo" width="50" height="50"/>
+                    <Typography variant="h4" style={{ marginLeft: 20}}>CS:GO Stat Tracker</Typography>
+                </Toolbar>
+            </Container>
         </AppBar>
     )
 }
