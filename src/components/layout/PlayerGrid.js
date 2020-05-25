3530f4e0b76d8b520c8a1485ab684222df04a7ed
chr-ge <ace26@protonmail.com>
2020-05-25 01:51:32 -0400

    Changed buttont variant and refactored component

--- src/components/layout/PlayerGrid.js
+++ src/components/layout/PlayerGrid.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useContext } from 'react';
 import { PlayerContext } from '../../context/PlayerContext';
+import { Link } from 'react-router-dom';
 import Player from '../Player';
 import PlayerStats from '../PlayerStats';
-import SearchAgainButton from '../SearchAgainButton';
 
 //material-ui
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
 const PlayerGrid = ({match}) => {
-    const { loading, player, searchPlayer } = useContext(PlayerContext);
+    const { loading, player, searchPlayer, resetPlayer } = useContext(PlayerContext);
 
     useEffect(() => {
         if(!player.platformUserId){
@@ -22,7 +23,17 @@ const PlayerGrid = ({match}) => {
             {!loading && <Grid container spacing={2}>
                 <Grid item sm={4}>
                     <Player />
-                    <SearchAgainButton />
+                    <Button
+                        component={Link}
+                        to="/"
+                        variant="outlined"
+                        color="primary"
+                        style={{ marginTop: '1rem'}}
+                        onClick={resetPlayer}
+                        fullWidth
+                    >
+                        Seach for another Player
+                    </Button>
                 </Grid>
                 <Grid item sm={8}>
                     <PlayerStats />
